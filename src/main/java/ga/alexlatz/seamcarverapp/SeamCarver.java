a01f8842853e47c99e54d88b0e1ebf10eae38b66
Alex Latz <alexlatz@pm.me>
2020-08-16 20:34:39 -0400

    Fixed the bulk seam generation, now onto making the seams removed and added more intelligently

--- src/main/java/ga/alexlatz/seamcarverapp/SeamCarver.java
+++ src/main/java/ga/alexlatz/seamcarverapp/SeamCarver.java
@@ -4,9 +4,7 @@ import javafx.scene.image.PixelReader;
 import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 
 public class SeamCarver {
     private WritableImage image;
@@ -46,7 +44,6 @@ public class SeamCarver {
         int[][] seams = new int[height()][num];
         boolean[][] usedPixels = new boolean[height()][width()];
         double[][] energyTo = new double[width()][height()];
-        ArrayList<HashSet<Integer>> possibleMoves = new ArrayList<>();
         for (int y = 0; y < height(); y++) {
             for (int x = 0; x < width(); x++) {
                 if (y == 0)
@@ -64,68 +61,54 @@ public class SeamCarver {
         }
         for (int i = 0; i < num; i++) {
             double min = Double.POSITIVE_INFINITY;
-            if (height() > 1) {
-                for (int x = 0; x < width(); x++) {
-                    if (energyTo[x][height() - 2] < min && !usedPixels[height() - 2][x]) {
-                        min = energyTo[x][height() - 2];
-                        seams[height() - 2][i] = x;
-                    }
+            for (int x = 0; x < width(); x++) {
+                if (energyTo[x][height() - 2] < min && !usedPixels[height() - 2][x]) {
+                    min = energyTo[x][height() - 2];
+                    seams[height() - 2][i] = x;
                 }
-                seams[height() - 1][i] = seams[height() - 2][i];
-                usedPixels[height() - 2][seams[height() - 2][i]] = true;
-                usedPixels[height() - 1][seams[height() - 2][i]] = true;
-            } else {
-                seams[height() - 1][i] = 0;
             }
-            possibleMoves.add(new HashSet<>());
-            possibleMoves.get(i).add(seams[height() - 2][i]);
-            if (seams[height() - 2][i] < width() - 1) possibleMoves.get(i).add(seams[height() - 2][i] + 1);
-            if (seams[height() - 2][i] > 0) possibleMoves.get(i).add(seams[height() - 2][i] - 1);
+            seams[height() - 1][i] = seams[height() - 2][i];
+            usedPixels[height() - 2][seams[height() - 2][i]] = true;
+            usedPixels[height() - 1][seams[height() - 2][i]] = true;
         }
-        for (int y = height() - 2; y > 0; y--) {
-            for (int i = 0; i < num; i++) {
+        for (int i = 0; i < num; i++) {
+            for (int y = height() - 2; y > 0; y--) {
                 double minSeam = Double.POSITIVE_INFINITY;
                 if (seams[y][i] - 1 > 0) {
-                    if (energyTo[seams[y][i] - 1][y - 1] < minSeam && !usedPixels[y - 1][seams[y][i] - 1] && allowedMove(possibleMoves, i, seams[y][i] - 1)) {
-                        minSeam = energyTo[seams[y][i] - 1][y - 1];
-                        seams[y - 1][i] = seams[y][i] - 1;
+                    int left = leftMove(usedPixels, seams[y][i], y - 1);
+                    if (left != -1 && energyTo[left][y - 1] < minSeam && !usedPixels[y - 1][left]) {
+                        minSeam = energyTo[left][y - 1];
+                        seams[y - 1][i] = left;
                     }
                 }
-                if (energyTo[seams[y][i]][y - 1] < minSeam && !usedPixels[y - 1][seams[y][i]] && allowedMove(possibleMoves, i, seams[y][i])) {
+                if (energyTo[seams[y][i]][y - 1] < minSeam && !usedPixels[y - 1][seams[y][i]]) {
                     minSeam = energyTo[seams[y][i]][y - 1];
                     seams[y - 1][i] = seams[y][i];
                 }
                 if (seams[y][i] + 1 < width()) {
-                    if (energyTo[seams[y][i] + 1][y - 1] < minSeam && !usedPixels[y - 1][seams[y][i] + 1] && allowedMove(possibleMoves, i, seams[y][i] + 1)) {
-                        seams[y - 1][i] = seams[y][i] + 1;
+                    int right = rightMove(usedPixels, seams[y][i], y - 1);
+                    if (right != -1 && energyTo[right][y - 1] < minSeam && !usedPixels[y - 1][right]) {
+                        seams[y - 1][i] = right;
                     }
                 }
-                if (seams[y - 1][i] == 0) {
-                    System.out.println("bad");
-                    int add = 0, sub = 0;
-                    while (seams[y][i] + add < width() || !usedPixels[y - 1][seams[y][i] + add]) add++;
-                    while (seams[y][i] - sub >= 0 || !usedPixels[y - 1][seams[y][i] - sub]) sub++;
-                    if (add > sub) seams[y - 1][i] = seams[y - 1][i] + add;
-                    else seams[y - 1][i] = seams[y - 1][i] - sub;
-                }
                 usedPixels[y - 1][seams[y - 1][i]] = true;
-                for (int j = i + 1; j < num; j++) {
-                    possibleMoves.get(j).remove(seams[y - 1][i]);
-                }
-                possibleMoves.get(i).clear();
-                possibleMoves.get(i).add(seams[y - 1][i]);
-                if (seams[y - 1][i] < width() - 1) possibleMoves.get(i).add(seams[y - 1][i] + 1);
-                if (seams[y - 1][i] > 0) possibleMoves.get(i).add(seams[y - 1][i] - 1);
             }
         }
         return seams;
     }
 
-    private boolean allowedMove(ArrayList<HashSet<Integer>> possibleMoves, int i, int x) {
-        for (int j = i + 1; j < possibleMoves.size(); j++) {
-            if (possibleMoves.get(j).contains(x) && possibleMoves.get(j).size() == 1) return false;
+    private int leftMove(boolean[][] usedPixels, int x, int y) {
+        for (int i = 1; i <= x; i++) {
+            if (!usedPixels[y][x - i]) return x - i;
         }
-        return true;
+        return -1;
+    }
+
+    private int rightMove(boolean[][] usedPixels, int x, int y) {
+        for (int i = 1; i < usedPixels[0].length - x; i++) {
+            if (!usedPixels[y][x + i]) return x + i;
+        }
+        return -1;
     }
 
     public int[][] findHorizontalSeam(int num) {
@@ -144,16 +127,20 @@ public class SeamCarver {
             Arrays.sort(seam[y]);
             int curRow = 0;
             for (int x = 0; x < width(); x++) {
-                if (Arrays.binarySearch(seam[y], x) < 0) {
-                    writer.setArgb(curRow, y, reader.getArgb(x, y));
-                    if (newEnergy[curRow][y] != 0) newEnergy[curRow++][y] = energy[x][y];
-                } else {
-                    newEnergy[curRow][y] = energy(curRow, y);
-                    if (curRow > 0) newEnergy[curRow - 1][y] = energy(curRow - 1, y);
-                }
+                if (Arrays.binarySearch(seam[y], x) < 0) writer.setArgb(curRow++, y, reader.getArgb(x, y));
             }
         }
         image = newPic;
+        for (int y = 0; y < energy[0].length; y++) {
+            int curRow = 0;
+            for (int x = 0; x < energy.length; x++) {
+                if (Arrays.binarySearch(seam[y], x) < 0) newEnergy[curRow++][y] = energy[x][y];
+                else {
+                    if (curRow > 0) newEnergy[curRow - 1][y] = energy(curRow - 1, y);
+                    if (curRow < newEnergy.length) newEnergy[curRow][y] = energy(curRow, y);
+                }
+            }
+        }
         energy = newEnergy;
     }
 
@@ -169,6 +156,7 @@ public class SeamCarver {
         final WritableImage newPic = new WritableImage(width() + seam[0].length, height());
         PixelWriter writer = newPic.getPixelWriter();
         PixelReader reader = image.getPixelReader();
+        double[][] newEnergy = new double[width() + seam[0].length][height()];
         for (int y = 0; y < height(); y++) {
             Arrays.sort(seam[y]);
             int curRow = 0;
@@ -198,70 +186,31 @@ public class SeamCarver {
             }
         }
         image = newPic;
-        seamEnergy(seam, false);
+        for (int y = 0; y < energy[0].length; y++) {
+            int curRow = 0;
+            for (int x = 0; x < energy.length; x++) {
+                if (Arrays.binarySearch(seam[y], x) < 0) newEnergy[curRow++][y] = energy[x][y];
+                else {
+                    if (x != energy.length - 1) {
+                        if (curRow > 0) newEnergy[curRow - 1][y] = energy(curRow - 1, y);
+                        newEnergy[curRow][y] = energy(curRow, y);
+                        if (curRow < newEnergy.length - 1) newEnergy[curRow + 1][y] = energy(curRow + 1, y);
+                    } else {
+                        newEnergy[curRow - 2][y] = energy(curRow - 2, y);
+                        newEnergy[curRow - 1][y] = energy(curRow - 1, y);
+                        newEnergy[curRow][y] = energy(curRow, y);
+                    }
+                    curRow += 2;
+                }
+            }
+        }
+        energy = newEnergy;
     }
 
     public void addHorizontalSeam(final int[][] seam) {
         transpose();
         addVerticalSeam(seam);
         transpose();
-    }
-
-    private void seamEnergy(int[][] seam, boolean delete) {
-        double[][] newEnergy = new double[width()][height()];
-        for (int y = 0; y < height(); y++) {
-            System.arraycopy(energy, 0, newEnergy, 0, seam[y][0]);
-            for (int i = 0; i < seam[0].length; i++) {
-                if (!delete) {
-                    newEnergy[seam[y][i]][y] = energy(seam[y][i], y);
-                    if (seam[y][i] < width() - 1) {
-                        newEnergy[seam[y][i] + 1][y] = energy(seam[y][i] + 1, y);
-                        newEnergy[seam[y][i] + 2][y] = energy(seam[y][i] + 2, y);
-                        if (i < seam[0].length - 1)
-                            System.arraycopy(energy, seam[y][i] + 2, newEnergy, seam[y][i] + 3, (seam[y][i + 1] - seam[y][i]));
-                        try {
-                            System.arraycopy(energy, seam[y][i] + 2, newEnergy, seam[y][i] + 3, energy.length - (seam[y][i]));
-                        } catch (ArrayIndexOutOfBoundsException e) {
-                            System.out.println("bad");
-                        }
-                    } else {
-                        newEnergy[seam[y][i] - 1][y] = energy(seam[y][i] - 1, y);
-                        newEnergy[seam[y][i] + 1][y] = energy(seam[y][i] + 1, y);
-                    }
-                } else {
-                    if (seam[y][i] > 0) newEnergy[seam[y][i] - 1][y] = energy(seam[y][i] - 1, y);
-                    if (seam[y][i] < width() - 1) {
-                        if (seam[y][i] < width() - 1) newEnergy[seam[y][i]][y] = energy(seam[y][i], y);
-                        if (i < seam[0].length - 1)
-                            System.arraycopy(energy, seam[y][i] + 2, newEnergy, seam[y][i] + 1, (seam[y][i + 1] - seam[y][i]));
-                        else
-                            System.arraycopy(energy, seam[y][i] + 2, newEnergy, seam[y][i] + 1, newEnergy.length - (seam[y][i]));
-                    }
-                }
-            }
-            /*
-            if (!delete) {
-                newEnergy[seam[y]][y] = energy(seam[y], y);
-                if (seam[y] < width() - 1) {
-                    newEnergy[seam[y] + 1][y] = energy(seam[y] + 1, y);
-                    newEnergy[seam[y] + 2][y] = energy(seam[y] + 2, y);
-                    if (energy.length - (seam[y] + 2) > -1)
-                        System.arraycopy(energy, seam[y] + 2, newEnergy, seam[y] + 3, energy.length - (seam[y] + 2));
-                } else {
-                    newEnergy[seam[y]-1][y] = energy(seam[y]-1, y);
-                    newEnergy[seam[y]+1][y] = energy(seam[y]+1, y);
-                }
-            } else {
-                if (seam[y] > 0) newEnergy[seam[y] - 1][y] = energy(seam[y] - 1, y);
-                if (seam[y] < width() - 1) {
-                    if (seam[y] < width() - 1) newEnergy[seam[y]][y] = energy(seam[y], y);
-                    if (seam[y] + 2 < energy.length)
-                        System.arraycopy(energy, seam[y] + 2, newEnergy, seam[y] + 1, newEnergy.length - (seam[y] + 1));
-                }
-            }
-             */
-        }
-        energy = newEnergy;
     }
 
     private void transpose() {
@@ -292,14 +241,5 @@ public class SeamCarver {
         final double g = ((c1 >> 8) & 0xff) - ((c2 >> 8) & 0xff);
         final double b = ((c1) & 0xff) - ((c2) & 0xff);
         return r * r + g * g + b * b;
-    }
-
-    private class Node {
-        Node left;
-        Node down;
-        Node right;
-        double energy;
-        int x;
-        int y;
     }
 }
